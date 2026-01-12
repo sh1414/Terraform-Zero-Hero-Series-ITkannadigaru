3abfc53a6b68e8707d0e1393aac0cc00b6965ddd
sh1414 <49478603+sh1414@users.noreply.github.com>
2026-01-12 23:02:17 +0530

    Update main.tf

--- 05-Count/main.tf
+++ 05-Count/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2" {
-    ami = "ami-07ff62358b87c7116"
+    ami = "ami-02dc6e3e481e2bbc5 "
     instance_type = "t3.micro"
-    count = 5
+    count = 3
 }
